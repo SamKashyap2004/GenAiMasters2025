e7bff3235ad00a6386bd3fafbdabb2d8e95fc829
SamKashyap2004 <sameerk326sm@gmail.com>
2025-06-08 22:25:07 +0530

    Question10 completed

---
+++ src/Qyestion12.kt
@@ -0,0 +1,9 @@
+fun main() {Add commentMore actions
+    val price = 6.5
+    val category = when {
+        price < 3 -> "Budget Brew!"
+        price in 3.0..6.0 -> "Moderate Mug!"
+        else -> "Premium Pour!"
+    }
+    println("Price category: $category")
+}
