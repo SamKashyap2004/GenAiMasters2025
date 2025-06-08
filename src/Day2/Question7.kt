6c9fa2bc16f73db5197028fc367d2a5108096514
SamKashyap2004 <sameerk326sm@gmail.com>
2025-06-08 22:54:46 +0530

    Question7 completed

---
+++ src/Day2/Question7.kt
@@ -0,0 +1,10 @@
+package Day2
+
+fun prepareCoffee(sugar: Int = 1) {Add commentMore actions
+    println("Preparing coffee with $sugar spoon(s) of sugar.")
+}
+
+fun main() {
+    prepareCoffee()
+    prepareCoffee(2)
+}
