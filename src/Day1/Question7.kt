ca1a5352798ac3efe33297dc25786c686fd55be1
SamKashyap2004 <sameerk326sm@gmail.com>
2025-06-08 22:18:37 +0530

    Question7 completed

---
+++ src/Day1/Question7.kt
@@ -0,0 +1,9 @@
+package Day1
+
+fun main() {Add commentMore actions
+    for (price in 1..10) {
+        if (price % 2 == 0) {
+            println("Even price: $$price coffee is available!")
+        }
+    }
+}
