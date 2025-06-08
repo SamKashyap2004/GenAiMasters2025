33b7ec6656a612b79996fc68593d1dd4890d3e39
SamKashyap2004 <sameerk326sm@gmail.com>
2025-06-08 22:31:03 +0530

    Question15 completed

---
+++ src/Day1/Question15.kt
@@ -0,0 +1,9 @@
+package Day1
+
+fun main() {Add commentMore actions
+    var inventory = 10
+    do {
+        println("Inventory: $inventory cups remaining")
+        inventory--
+    } while (inventory > 0)
+}
