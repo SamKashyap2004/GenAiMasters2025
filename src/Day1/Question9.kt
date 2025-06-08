951c4f03c776a89b2972a30b03068521ea7892c1
SamKashyap2004 <sameerk326sm@gmail.com>
2025-06-08 22:20:57 +0530

    Question9 completed

---
+++ src/Day1/Question9.kt
@@ -0,0 +1,9 @@
+package Day1
+
+fun main() {Add commentMore actions
+    val age = 61
+    if (age >= 60)
+        println("Eligible for the senior coffee discount, Enjoy!")
+    else
+        println("Sorry, no discount yet!")
+}
