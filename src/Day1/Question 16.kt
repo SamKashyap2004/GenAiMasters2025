6957eac8b3604d449906abb75e207ca099e5496c
SamKashyap2004 <sameerk326sm@gmail.com>
2025-06-08 22:31:54 +0530

    Question16 completed

---
+++ src/Day1/Question 16.kt
@@ -0,0 +1,9 @@
+package Day1
+
+fun main() {Add commentMore actions
+    val orderName = "Latte"
+    if (orderName.length >= 5)
+        println("Order name '$orderName' is valid!")
+    else
+        println("Order name '$orderName' is too short!")
+}
